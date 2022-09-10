eaa5efa0953bee2894475a18ef0f4d0f38a213ca
Александр <muksaflash@gmail.com>
2022-09-10 13:39:12 +0300

    Пройдены внутренние тесты

--- StatisticsTask.cs
+++ StatisticsTask.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,16 +9,36 @@ namespace linq_slideviews
 		public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
 		{
 			var tempVisitGroups = visits
-			   //.Where(visit => visit.SlideType == slideType)
 			   .GroupBy(visit => visit.UserId);
 			var visitGroups = tempVisitGroups.Select(visitGroup => visitGroup.OrderBy(x => x.DateTime));
-			var j = visitGroups.Select(visitGroup => visitGroup
-			.Select(visit => (double)visit.DateTime.Minute)
-			.Bigrams()
-			.Where(t => t.Item2 - t.Item1 >= 1 && t.Item2 - t.Item1 <= 120));
-			var r = visitGroups.Select(visitGroup => visitGroup.Where(t => visitGroup.Select));
-			//var result = j.SelectMany(x => x.Select(t => t.Item1)).Median();
-            return 2.0;
+			var timeBigramGroups = visitGroups.Select(visitGroup => visitGroup
+			.Select(visit => visit.DateTime.TimeOfDay.TotalMinutes)
+			.Bigrams());
+			var slideTypes = visitGroups.SelectMany(visitGroup => visitGroup.Select(visit => visit.SlideType)).ToList();
+			var typeTimeTupleGroups = timeBigramGroups
+				.Select(
+					timeBigramGroup => timeBigramGroup
+					.Select(
+						timeBigram =>
+						{ 
+							var resultTuple = Tuple.Create(slideTypes[0], timeBigram);
+							slideTypes.RemoveAt(0);
+							return resultTuple;
+						}));
+			var readyTupleGroups = typeTimeTupleGroups
+				.Select(
+					typeTimeTupleGroup => typeTimeTupleGroup
+					.Where(x => x.Item2.Item2 - x.Item2.Item1 >= 1 && x.Item2.Item2 - x.Item2.Item1 <= 120)
+					.Where(x => x.Item1 == slideType)
+					.Where(x => x != null));
+			double result = 0;
+			if (readyTupleGroups.Any())
+				try
+				{
+					result = readyTupleGroups.SelectMany(x => x.Select(t => t.Item2.Item2 - t.Item2.Item1)).Median();
+				}
+				catch { return 0.0; }
+			return result;
 		}
 	}
 }
